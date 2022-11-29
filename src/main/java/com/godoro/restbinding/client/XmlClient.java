b7127f2cda52a5ca28d5ab86e94a6442fc501ee8
cem kok <cem.kok@32bit.com>
2022-11-29 11:27:39 +0300

    Xml Client Updated

--- src/main/java/com/godoro/restbinding/client/XmlClient.java
+++ src/main/java/com/godoro/restbinding/client/XmlClient.java
@@ -1,10 +1,17 @@
 package com.godoro.restbinding.client;
 
+import java.io.ByteArrayInputStream;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.godoro.restbinding.document.Product;
 
 public class XmlClient {
 public static void main(String[] args) {
@@ -23,11 +30,36 @@ public static void main(String[] args) {
 		try {
 			DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder documentBuilder = builderFactory.newDocumentBuilder();
+			ByteArrayInputStream inputStream= new ByteArrayInputStream(plain.getBytes());
+			Document document = documentBuilder.parse(inputStream);
+			inputStream.close();
+			
+			Element product=document.getDocumentElement();
+//			long productId=Long.parseLong(product.getAttribute("productId"));
+//			System.out.println("productId :" + productId);
+			long productId = Long.parseLong(
+					product.getChildNodes().item(0).getTextContent());
+			String productName = product.getChildNodes().item(1).getTextContent();
+			double salesPrice =Double.parseDouble(
+					product.getChildNodes().item(2).getTextContent());
+			
+			System.out.println("Ürün" +  productId+" " + productName + " "+ salesPrice);
+			
+			
+	
 		
 			
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
+		
+		//Object alma
+		Product product=target.request("application/xml; charset=UTF-8")
+				.get(Product.class);
+		System.out.println("Ürün" +  product.getProductId()+" " + product.getProductName() + " "
+				+ product.getSalesPrice());
+		
+			
 		
 		
 			
